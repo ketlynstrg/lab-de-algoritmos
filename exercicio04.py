4295f962e5d0e9c25fc1a2cf15c90cc1dae2ac28
ketlyn luana <161777843+ketlynstrg@users.noreply.github.com>
2024-03-19 13:47:19 -0300

    Create exercicio04.py

---
+++ exercicio04.py
@@ -0,0 +1,7 @@
+carro = int(input("insira a quantidade de carros:"))
+pneu = carro *4 
+valor= pneu*395.40 
+estepe=carro*395.40
+estepeFinal= estepe+valor
+print("valor total da troca de pneus é:", valor)
+print("caso os estepes também sejam trocados, o valor final passa a ser", estepeFinal)
